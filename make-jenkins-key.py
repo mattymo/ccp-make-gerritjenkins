93139182e198334f3e2e3f506a54bd94ecc32aef
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-09-01 12:13:33 +0200

    Add jenkins creds by ssh key file script
    Expects an ssh private key is placed at
    the Jenkins host.
    
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

---
+++ make-jenkins-key.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import logging
+import os
+from jenkinsapi.jenkins import Jenkins
+from jenkinsapi.credential import SSHKeyCredential
+
+log_level = getattr(logging, 'DEBUG')
+logging.basicConfig(level=log_level)
+logger = logging.getLogger()
+
+jenkins_url = os.environ.get('JENKINS_URL', 'http://ci.mcp-ci.local:8082/jenkins')
+secret_file = os.environ.get('SECRET_FILE', '/home/jenkins/.ssh/jenkins_mcp')
+passphrase = os.environ.get('PASSPHRASE', '')
+creds_id = os.environ.get('CLUSTERNAME', 'dummy-cluster-name')
+admin_user = os.environ.get('ADMIN_USER', 'admin')
+admin_pass = os.environ.get('ADMIN_PASS', 'secret')
+
+api = Jenkins(jenkins_url, username=admin_user, password=admin_pass)
+
+# Get a list of all global credentials
+creds = api.credentials
+logging.info(api.credentials.keys())
+
+creds_desc = 'credentials for Jenkins to access Kubernetes nodes'
+cred_dict = {
+    'description': creds_desc,
+    'userName': '{0}-cluster-key'.format(creds_id),
+    'passphrase': passphrase,
+    'private_key': secret_file
+}
+creds[creds_desc] = SSHKeyCredential(cred_dict)
