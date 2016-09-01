e9c56f6132eef82538ecac9bd587259b04b02bae
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-09-01 12:52:36 +0300

    Switch to jenkins job template and create cluster jobs in 1 project

--- make-jenkins-jobs.sh
+++ make-jenkins-jobs.sh
@@ -6,10 +6,10 @@ WORKDIR=$(mktemp ./gerrit-resources-XXX)
 GIT_TOPIC="add-$CLUSTERNAME"
 
 cd $WORKDIR
-git clone https://review.fuel-infra.org/fuel-infra/project-config
-#TODO
-#Something like this:
-# for file in jjb/*.j2;do j2 -f env $file ./config > project-config/jobs/dst;done
-# copy other files
-# add all
-# git review
+git clone "$gerrit_server/fuel-infra/project-config"
+cp -R jjb/common/* project-config/jobs/common/
+j2 -f env jjb/newcluster.yaml.j2 ./config > project-config/jobs/clusters/$CLUSTERNAME.yaml
+cd project-config
+git add *
+git commit -a -m "New project $CLUSTERNAME"
+git review -t "add_cluster_$CLUSTERNAME"
